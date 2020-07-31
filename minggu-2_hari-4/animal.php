0a7f85d03a5635119b0953a27d639729c2eb86e0
abiadhi666 <54876312+abiadhi666@users.noreply.github.com>
2020-07-31 14:33:58 +0700

    Add files via upload

---
+++ minggu-2_hari-4/animal.php
@@ -0,0 +1,18 @@
+<?php
+
+class Animal
+{
+  public $legs = 2;
+  public $cold_blooded = false;
+  public $name;
+
+  function __construct($name) {
+    $this->name = $name;
+  }
+  function getName() {
+    echo $this->name;
+  }
+  function getLegs() {
+    echo $this->legs;
+  }
+}
