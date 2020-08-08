7109b48eb80db0b9feb8fb3b59a1cf0a738202d4
abi@pusilkom.com <abi@pusilkom.com>
2020-08-08 11:46:20 +0700

    Update .env.example, AppServiceProvider.php, and 11 more files...

--- minggu-3_hari-5/blog/app/Providers/AppServiceProvider.php
+++ minggu-3_hari-5/blog/app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Schema\Builder; // Import Builder where defaultStringLength method is defined
+
 use Illuminate\Support\ServiceProvider;
+
+function boot()
+{
+    Builder::defaultStringLength(191); // Update defaultStringLength
+}
 
 class AppServiceProvider extends ServiceProvider
 {
